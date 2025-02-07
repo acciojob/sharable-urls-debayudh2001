90517b15665339e3345ff4719201bb1b0dd7ff12
debayudh2001 <152640749+debayudh2001@users.noreply.github.com>
2025-02-08 02:30:31 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,23 @@
 // your code here
+let btn = document.querySelector("button")
+let name = document.querySelector("#name")
+let year = document.querySelector("#year")
+btn.addEventListener("click", (e) => {
+	e.preventDefault()
+	let heading = document.querySelector("h3")
+	if(name.value && year.value){
+		heading.innerText = heading.innerText + `?name=${name.value}&year=${year.value}`
+		name.value = ""
+		year.value = ""
+	}
+	if(name.value){
+		heading.innerText = heading.innerText + `?name=${name.value}`
+		name.value = ""
+	}
+	if(year.value){
+		heading.innerText = heading.innerText + `?year=${year.value}`
+		year.value = ""
+	}
+})
+
+
